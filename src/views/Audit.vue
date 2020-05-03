e9401fa9996cfecbc1a4c319cff5a17ade7065e3
AnimatorJoe <josephjin7@gmail.com>
2020-05-03 14:54:44 -0500

    allowing users to login with gmail

---
+++ src/views/Audit.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+    </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      email: null,
+      password: null,
+      error: null
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div v-if="error" class="error">{{erorr.message}}</div>
+        <form class="general-form" @submit.prevent="login">
+            <div class="form-item">Login</div>
+            <!-- <div class="form-item"><input type="email" v-model="email" placeholder="noobmaster69@protonmail.com"></div> -->
+            <!-- <div class="form-item"><input type="password" v-model="password" placeholder="password"></div> -->
+            <div class="form-item"><button class="button" type="submit">Login</button></div>
+        </form>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+require('@/assets/styles/forms.css')
+
+export default {
+  data () {
+    return {
+      email: null,
+      password: null,
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        const res = await firebase.auth().signInWithPopup(provider)
+        console.log('user logged in')
+        console.log(res)
+      } catch (err) {
+        console.log('Error loggin in')
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
