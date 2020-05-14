c3d595810be929214096a23db9c7f751cc72d5f0
AnimatorJoe <josephjin7@gmail.com>
2020-05-13 23:12:28 -0500

    adding a navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <v-toolbar depressed app class="text-uppercase secondary--text">
+            <span class = "font-weight-light">PinIt</span>
+            <span class = "font-weight-bold">Auditor</span>
+            <v-spacer />
+            <v-btn depressed>
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
