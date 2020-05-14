eeab94e766bd042b01e8a51798c539613bd18ee2
AnimatorJoe <josephjin7@gmail.com>
2020-05-14 15:25:20 -0500

    slight changes to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
             <span class = "font-weight-light title">PinIt</span>
             <span class = "font-weight-bold title">Auditor</span>
             <v-spacer />
-            <v-btn depressed class="primary white--text">
+            <v-btn depressed class="primary white--text" v-show="currentUser" @click="logout">
                 <span>Sign Out</span>
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
@@ -13,7 +13,18 @@
 </template>
 
 <script>
-export default {
+import firebase from 'firebase'
 
+export default {
+  computed: {
+    currentUser () {
+      return this.$store.state.currentUser
+    }
+  },
+  methods: {
+    async logout () {
+      await firebase.auth().signOut()
+    }
+  }
 }
 </script>
