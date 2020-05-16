bf18c3352a4e78424f67eda12680f7b239a853f5
AnimatorJoe <josephjin7@gmail.com>
2020-05-15 23:13:11 -0500

    made data table rows expandable and adding refresh button to auditor

--- src/views/Audit.vue
+++ src/views/Audit.vue
@@ -1,37 +1,45 @@
 <template>
-    <v-data-table
-      :headers="headers"
-      sort-by="date"
-      :sort-desc="[true]"
-      :items="pendingPosts">
-      <template v-slot:item.view="{ item }">
-        <v-icon
-          small
-          class="mr-2"
-          @click="viewMore(item)"
-        >
-          mdi-clipboard-account
-        </v-icon>
-      </template>
-      <template v-slot:item.approve="{ item }">
-        <v-icon
-          small
-          class="mr-2"
-          @click="approve(item)"
-        >
-          mdi-check
-        </v-icon>
-      </template>
-      <template v-slot:item.delete="{ item }">
-        <v-icon
-          small
-          class="mr-2"
-          @click="deleteItem(item)"
-        >
-          mdi-trash-can-outline
-        </v-icon>
-      </template>
-    </v-data-table>
+  <v-data-table
+    :headers="headers"
+    :sort-desc="[true]"
+    :items="pendingPosts"
+    sort-by="date"
+    show-expand
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Pending User Posts</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn depressed class="tertiary" @click="loadPosts()">
+            <span>refresh</span>
+            <v-icon right>loop</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </template>
+
+    <template v-slot:item.approve="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="approve(item)"
+      >
+        mdi-check
+      </v-icon>
+    </template>
+    <template v-slot:item.delete="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="deleteItem(item)"
+      >
+        mdi-trash-can-outline
+      </v-icon>
+    </template>
+    <template v-slot:expanded-item="{ headers, item }">
+      <td :colspan="headers.length">More info about {{ item.title }}</td>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
@@ -39,24 +47,18 @@ import manager from '@/api/firebase/post-manager.js'
 
 export default {
   async created () {
-    this.pendingPosts = await manager.fetchPendingPosts()
+    await this.loadPosts()
   },
   data () {
     return {
       headers: [
         {
           text: 'Title',
-          value: 'title',
-          sortable: false
+          value: 'title'
         },
         {
           text: 'Date',
           value: 'timestamp.seconds'
-        },
-        {
-          text: 'View More',
-          value: 'view',
-          sortable: false
         },
         {
           text: 'Approve',
@@ -69,19 +71,14 @@ export default {
           sortable: false
         }
       ],
-      pins: [
-        {
-          title: 'sssssss',
-          timestamp: {
-            seconds: 'sksksksksk'
-          }
-        }
-      ],
       pendingPosts: [
       ]
     }
   },
   methods: {
+    async loadPosts () {
+      this.pendingPosts = await manager.fetchPendingPosts()
+    },
     viewMore (item) {
     },
     deleteItem (item) {
